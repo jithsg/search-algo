2e5f4fc8ebae053cb870d7a027002b41ace304e7
jithsg <135303624+jithsg@users.noreply.github.com>
2025-04-13 02:06:59 +0800

    insertion

---
+++ insertion.py
@@ -0,0 +1,15 @@
+def insertion(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        
+        while j >=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+            
+        arr[j+1] = key
+    return arr
+
+
+
+print(insertion([5, -1, 4, -3]))
