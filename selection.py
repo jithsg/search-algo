6d7ef0175523e59f83fddba94f82359f25266f8b
jithsg <135303624+jithsg@users.noreply.github.com>
2025-04-12 17:44:34 +0800

    minimum

--- selection.py
+++ selection.py
@@ -10,3 +10,16 @@ def selection_sort(arr):
 
 
 print(selection_sort([5, 4, 3, 2, 1]))
+
+# def minimum(arr):
+#     for i in range(len(arr)):
+#         min_idx = i
+#         for j in range(i+1, len(arr)):
+#             if arr[j] < arr[min_idx]:
+#                 min_idx = j
+#         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+#     return arr
+    
+
+
+# minimum([4, -3, 2, -1])
