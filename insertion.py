ee9f0e85dc490d09e0c7a49282d40961e208005b
jithsg <135303624+jithsg@users.noreply.github.com>
2025-04-17 02:54:18 +0800

    is

--- insertion.py
+++ insertion.py
@@ -1,13 +1,13 @@
 def insertion(arr):
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i-1
+        j = i
         
-        while j >=0 and arr[j] > key:
-            arr[j+1] = arr[j]
+        while j > 0 and  key <arr[j-1]:
+            arr[j] = arr[j-1]
             j -= 1
             
-        arr[j+1] = key
+        arr[j] = key
     return arr
 
 
